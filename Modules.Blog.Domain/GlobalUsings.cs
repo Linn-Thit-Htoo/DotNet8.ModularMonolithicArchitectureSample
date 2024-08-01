910c746b74b3bdb12030964f4e98accce67eed71
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-02 01:51:01 +0630

    Format Code

--- Modules.Blog.Domain/GlobalUsings.cs
+++ Modules.Blog.Domain/GlobalUsings.cs
@@ -2,3 +2,5 @@
 
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
+global using Shared.DTOs.Features;
+global using Shared.DTOs.Features.Blog;
--- Modules.Blog.Domain/Interfaces/IBlogService.cs
+++ Modules.Blog.Domain/Interfaces/IBlogService.cs
@@ -1,18 +1,9 @@
-﻿using Shared.DTOs.Features;
-using Shared.DTOs.Features.Blog;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Blog.Domain.Interfaces;
 
-namespace Modules.Blog.Domain.Interfaces
+public interface IBlogService
 {
-    public interface IBlogService
-    {
-        Task<Result<BlogListResponseModel>> GetBlogList();
-        Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel);
-        Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id);
-        Task<Result<BlogResponseModel>> DeleteBlog(int id);
-    }
+    Task<Result<BlogListResponseModel>> GetBlogList();
+    Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel);
+    Task<Result<BlogResponseModel>> UpdateBlog(BlogRequestModel requestModel, int id);
+    Task<Result<BlogResponseModel>> DeleteBlog(int id);
 }
