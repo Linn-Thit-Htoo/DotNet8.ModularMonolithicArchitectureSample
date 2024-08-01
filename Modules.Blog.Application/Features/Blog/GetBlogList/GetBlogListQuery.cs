dda9f8d9c93c8d9dd72146dd96bb70d9c552b8a5
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-02 02:12:35 +0630

    Format Code

--- Modules.Blog.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
+++ Modules.Blog.Application/Features/Blog/GetBlogList/GetBlogListQuery.cs
@@ -1,12 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Modules.Blog.Application.Features.Blog.GetBlogList;
 
-namespace Modules.Blog.Application.Features.Blog.GetBlogList
-{
-    public class GetBlogListQuery : IRequest<Result<BlogListResponseModel>>
-    {
-    }
-}
+public class GetBlogListQuery : IRequest<Result<BlogListResponseModel>> { }
