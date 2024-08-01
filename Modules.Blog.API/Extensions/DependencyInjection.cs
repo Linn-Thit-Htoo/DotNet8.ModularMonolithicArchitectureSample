12b599aec635f288b34febfc7c3743ad29bfb1f2
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-02 01:48:34 +0630

    Format Code

--- Modules.Blog.API/Extensions/DependencyInjection.cs
+++ Modules.Blog.API/Extensions/DependencyInjection.cs
@@ -1,32 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Modules.Blog.Application.Services;
-using Modules.Blog.Infrastructure.Db;
+﻿namespace Modules.Blog.API.Extensions;
 
-namespace Modules.Blog.API.Extensions
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddDependencyInjection(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
     {
-        public static IServiceCollection AddDependencyInjection(this IServiceCollection services, WebApplicationBuilder builder)
-        {
-            return services.AddDbContextService(builder)
-                .AddBlogService();
-        }
+        return services.AddDbContextService(builder).AddBlogService();
+    }
 
-        private static IServiceCollection AddDbContextService(this  IServiceCollection services, WebApplicationBuilder builder)
-        {
-            builder.Services.AddDbContext<BlogDbContext>(opt =>
+    private static IServiceCollection AddDbContextService(
+        this IServiceCollection services,
+        WebApplicationBuilder builder
+    )
+    {
+        builder.Services.AddDbContext<BlogDbContext>(
+            opt =>
             {
                 opt.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
                 opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-            }, ServiceLifetime.Transient);
+            },
+            ServiceLifetime.Transient
+        );
 
-            return services;
-        }
+        return services;
+    }
 
-        private static IServiceCollection AddBlogService(this IServiceCollection services)
-        {
-            return services.AddScoped<IBlogService, BlogService>();
-        }
+    private static IServiceCollection AddBlogService(this IServiceCollection services)
+    {
+        return services.AddScoped<IBlogService, BlogService>();
     }
 }
--- Modules.Blog.API/GlobalUsings.cs
+++ Modules.Blog.API/GlobalUsings.cs
@@ -1,5 +1,8 @@
 // Global using directives
 
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Modules.Blog.Application.Services;
 global using Modules.Blog.Domain.Interfaces;
+global using Modules.Blog.Infrastructure.Db;
 global using Shared.DTOs.Features.Blog;
