21a84d8f367ae4ae104b349da696f21271a65c2a
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-02 01:48:14 +0630

    Format Code

--- Modules.Blog.API/Controllers/BlogController.cs
+++ Modules.Blog.API/Controllers/BlogController.cs
@@ -1,42 +1,41 @@
-﻿namespace Modules.Blog.API.Controllers
+﻿namespace Modules.Blog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogController : ControllerBase
+    private readonly IBlogService _blogService;
+
+    public BlogController(IBlogService blogService)
     {
-        private readonly IBlogService _blogService;
+        _blogService = blogService;
+    }
 
-        public BlogController(IBlogService blogService)
-        {
-            _blogService = blogService;
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetBlogs()
+    {
+        var result = await _blogService.GetBlogList();
+        return Ok(result);
+    }
 
-        [HttpGet]
-        public async Task<IActionResult> GetBlogs()
-        {
-            var result = await _blogService.GetBlogList();
-            return Ok(result);
-        }
+    [HttpPost]
+    public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
+    {
+        var result = await _blogService.CreateBlog(requestModel);
+        return Ok(result);
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateBlog([FromBody] BlogRequestModel requestModel)
-        {
-            var result = await _blogService.CreateBlog(requestModel);
-            return Ok(result);
-        }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBlog([FromBody] BlogRequestModel requestModel, int id)
+    {
+        var result = await _blogService.UpdateBlog(requestModel, id);
+        return Ok(result);
+    }
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateBlog([FromBody] BlogRequestModel requestModel, int id)
-        {
-            var result = await _blogService.UpdateBlog(requestModel, id);
-            return Ok(result);
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBlog(int id)
-        {
-            var result = await _blogService.DeleteBlog(id);
-            return Ok(result);
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBlog(int id)
+    {
+        var result = await _blogService.DeleteBlog(id);
+        return Ok(result);
     }
 }
