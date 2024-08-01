502087c675873d5169913c939d9484dd24f14d53
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-02 01:46:00 +0630

    Format Code

--- Shared.Domain/Enums/EnumStatusCode.cs
+++ Shared.Domain/Enums/EnumStatusCode.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Shared.Domain.Enums;
 
-namespace Shared.Domain.Enums
+public enum EnumStatusCode
 {
-    public enum EnumStatusCode
-    {
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        NoContent = 204,
-        BadRequest = 400,
-        UnAuthorized = 401,
-        NotFound = 404,
-        Conflict = 409,
-        InternalServerError = 500
-    }
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    NoContent = 204,
+    BadRequest = 400,
+    UnAuthorized = 401,
+    NotFound = 404,
+    Conflict = 409,
+    InternalServerError = 500
 }
